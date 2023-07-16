60a5f965854e88580690856af0e01255f50a2aa9
erion <erionrb@gmail.com>
2023-07-16 16:04:42 -0300

    added simple mint & transfer with access roles

--- contracts/Digitoken.sol
+++ contracts/Digitoken.sol
@@ -1,89 +1,135 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import {IERC1410} from "./interfaces/IERC1410.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import { IERC1410 } from "./interfaces/IERC1410.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import { Counters } from "@openzeppelin/contracts/utils/Counters.sol";
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+import { console } from "hardhat/console.sol";
 
-contract DigitokenFinal is IERC1410, ERC20, Ownable {
-    constructor() ERC20("Digitoken", "DIGI") {}
+contract Digitoken is IERC1410, ERC20, Ownable, AccessControl {
+    using Counters for Counters.Counter;
+    Counters.Counter private _burnerCounter;
 
-    function balanceOfByPartition(
-        bytes32 _partition,
-        address _tokenHolder
-    ) external view returns (uint256) {}
+    /**
+     * Access Roles
+     */
+    bytes32 public constant MINTER = keccak256("MINTER_ROLE");
+    bytes32 public constant TRANSFER_AGENT = keccak256("TRANSFER_AGENT_ROLE");
+    bytes32 public constant WHITE_LISTED = keccak256("WHITE_LISTED");
+    bytes32 public BURNER;
 
-    function partitionsOf(
-        address _tokenHolder
-    ) external view returns (bytes32[] memory) {}
+    constructor() ERC20("Digitoken", "DIGI") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(WHITE_LISTED, msg.sender);
+        setupBurner();
+    }
 
-    function transferByPartition(
-        bytes32 _partition,
-        address _to,
-        uint256 _value,
-        bytes memory _data
-    ) external returns (bytes32) {}
+    /**
+     *  Modifiers
+     */
+    modifier onlyAdmin() {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Sender is not an admin");
+        _;
+    }
 
-    function operatorTransferByPartition(
-        bytes32 _partition,
-        address _from,
-        address _to,
-        uint256 _value,
-        bytes memory _data,
-        bytes memory _operatorData
-    ) external returns (bytes32) {}
+    modifier onlyMinter() {
+        require(hasRoleOrAdmin(WHITE_LISTED, msg.sender), "Sender has not whitelisted");
+        require(hasRoleOrAdmin(MINTER, msg.sender), "Sender is not a minter");
+        _;
+    }
 
-    function canTransferByPartition(
-        address _from,
-        address _to,
-        bytes32 _partition,
-        uint256 _value,
-        bytes memory _data
-    ) external view returns (bytes memory, bytes32, bytes32) {}
+    modifier onlyBurner() {
+        require(hasRoleOrAdmin(WHITE_LISTED, msg.sender), "Sender has not whitelisted");
+        require(hasRoleOrAdmin(BURNER, msg.sender), "Sender is not a burner");
+        _;
+    }
 
-    // Operator Information
-    function isOperator(
-        address _operator,
-        address _tokenHolder
-    ) external view returns (bool) {}
+    modifier onlyTransferAgent(address to) {
+        address from = msg.sender;
+        require(hasRoleOrAdmin(WHITE_LISTED, to), "Recipient has not whitelisted");
+        require(hasRoleOrAdmin(TRANSFER_AGENT, to), "Recipient is not a transfer agent");
 
-    function isOperatorForPartition(
-        bytes32 _partition,
-        address _operator,
-        address _tokenHolder
-    ) external view returns (bool) {}
+        require(hasRoleOrAdmin(WHITE_LISTED, from), "Sender has not whitelisted");
+        require(hasRoleOrAdmin(TRANSFER_AGENT, from), "Sender is not a transfer agent");
+        _;
+    }
 
-    function authorizeOperator(address _operator) external {}
+    /**
+     * Functions
+     */
 
-    function revokeOperator(address _operator) external {}
+    function mint(address to, uint256 amount) public onlyMinter {
+        _mint(to, amount);
+    }
 
-    function authorizeOperatorByPartition(
-        bytes32 _partition,
-        address _operator
-    ) external {}
+    function transfer(address to, uint256 amount) public override onlyTransferAgent(to) returns (bool) {
+        super.transfer(to, amount);
+        return true;
+    }
 
-    function revokeOperatorByPartition(
-        bytes32 _partition,
-        address _operator
-    ) external {}
+    function setupBurner() public onlyAdmin {
+        BURNER = keccak256(abi.encodePacked("BURNER_ROLE_", Strings.toString(_burnerCounter.current())));
+        _burnerCounter.increment();
+    }
 
-    function issueByPartition(
-        bytes32 _partition,
-        address _tokenHolder,
-        uint256 _value,
-        bytes memory _data
-    ) external {}
+    function balanceOfByPartition(bytes32 _partition, address _tokenHolder) external view returns (uint256) {
+        // TODO: implement me
+    }
 
-    function redeemByPartition(
-        bytes32 _partition,
-        uint256 _value,
-        bytes memory _data
-    ) external {}
+    function partitionsOf(address _tokenHolder) external view returns (bytes32[] memory) {
+        // TODO: implement me
+    }
 
-    function operatorRedeemByPartition(
-        bytes32 _partition,
-        address _tokenHolder,
-        uint256 _value,
-        bytes memory _operatorData
-    ) external {}
+    function transferByPartition(bytes32 _partition, address _to, uint256 _value, bytes memory _data) external returns (bytes32) {
+        // TODO: implement me
+    }
+
+    function operatorTransferByPartition(bytes32 _partition, address _from, address _to, uint256 _value, bytes memory _data, bytes memory _operatorData) external returns (bytes32) {
+        // TODO: implement me
+    }
+
+    function canTransferByPartition(address _from, address _to, bytes32 _partition, uint256 _value, bytes memory _data) external view returns (bytes memory, bytes32, bytes32) {
+        // TODO: implement me
+    }
+
+    function isOperator(address _operator, address _tokenHolder) external view returns (bool) {
+        // TODO: implement me
+    }
+
+    function isOperatorForPartition(bytes32 _partition, address _operator, address _tokenHolder) external view returns (bool) {}
+
+    function authorizeOperator(address _operator) external {
+        // TODO: implement me
+    }
+
+    function revokeOperator(address _operator) external {
+        // TODO: implement me
+    }
+
+    function authorizeOperatorByPartition(bytes32 _partition, address _operator) external {
+        // TODO: implement me
+    }
+
+    function revokeOperatorByPartition(bytes32 _partition, address _operator) external {
+        // TODO: implement me
+    }
+
+    function issueByPartition(bytes32 _partition, address _tokenHolder, uint256 _value, bytes memory _data) external {
+        // TODO: implement me
+    }
+
+    function redeemByPartition(bytes32 _partition, uint256 _value, bytes memory _data) external {
+        // TODO: implement me
+    }
+
+    function operatorRedeemByPartition(bytes32 _partition, address _tokenHolder, uint256 _value, bytes memory _operatorData) external {
+        // TODO: implement me
+    }
+
+    function hasRoleOrAdmin(bytes32 role, address account) internal view returns (bool) {
+        return (hasRole(role, account)) || hasRole(DEFAULT_ADMIN_ROLE, account);
+    }
 }
---
+++ test/Digitoken.ts
@@ -0,0 +1,129 @@
+import { ethers } from "hardhat";
+import { BytesLike } from "ethers";
+import { expect } from "chai";
+import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
+import { Digitoken } from "../typechain-types";
+
+describe("Digitoken", () => {
+    let digitoken: Digitoken;
+
+    let owner: SignerWithAddress, user1: SignerWithAddress, user2: SignerWithAddress, minter: SignerWithAddress, transferAgent: SignerWithAddress, burner: SignerWithAddress;
+    let ownerAddress: string, user1Address: string, user2Address: string, minterAddress: string, transferAgentAddress: string, burnerAddress: string;
+
+    let WHITE_LISTED: BytesLike, ADMIN: BytesLike, MINTER: BytesLike, TRANSFER_AGENT: BytesLike;
+
+    before(async () => {
+        [owner, minter, transferAgent, burner, user1, user2] = await ethers.getSigners();
+        ownerAddress = await owner.getAddress();
+        user1Address = await user1.getAddress();
+        user2Address = await user2.getAddress();
+        minterAddress = await minter.getAddress();
+        transferAgentAddress = await transferAgent.getAddress();
+        burnerAddress = await burner.getAddress();
+
+        const Digitoken = await ethers.getContractFactory("Digitoken");
+
+        digitoken = await Digitoken.deploy();
+        await digitoken.waitForDeployment();
+
+        WHITE_LISTED = await digitoken.WHITE_LISTED();
+        ADMIN = await digitoken.DEFAULT_ADMIN_ROLE();
+        MINTER = await digitoken.MINTER();
+        TRANSFER_AGENT = await digitoken.TRANSFER_AGENT();
+    });
+
+    describe("Deployment", () => {
+        it("should set the correct owner", async () => {
+            const contractOwner = await digitoken.owner();
+            expect(contractOwner).to.equal(await owner.getAddress());
+        });
+
+        it("should whitelist & admin the owner", async () => {
+            const isAdminRole = await digitoken.connect(owner).hasRole(ADMIN, await ownerAddress);
+            const isWhitelisted = await digitoken.connect(owner).hasRole(WHITE_LISTED, owner);
+            expect(isWhitelisted, "Owner is not WHITELISTED").to.be.true;
+            expect(isAdminRole, "Owner is not ADMIM").to.be.true;
+        });
+    });
+
+    describe("Role Access", () => {
+        it("should allow admin to whitelist a user1", async () => {
+            expect(await digitoken.connect(owner).hasRole(ADMIN, owner), `owner[${ownerAddress}] is not ADMIN`).to.be.true;
+            expect(await digitoken.connect(owner).hasRole(WHITE_LISTED, owner), `owner[${ownerAddress}] is not WHITELISTED`).to.be.true;
+            expect(await digitoken.connect(owner).hasRole(WHITE_LISTED, user1), `user1[${user1Address}] is WHITELISTED`).to.be.false;
+
+            await digitoken.connect(owner).grantRole(WHITE_LISTED, user1Address);
+            expect(await digitoken.connect(owner).hasRole(WHITE_LISTED, user1), `user1[${user1Address}] has not WHITELISTED by owner[${ownerAddress}]`).to.be.true;
+        });
+
+        it("should allow admin to whitelist a user2", async () => {
+            expect(await digitoken.connect(owner).hasRole(ADMIN, owner), `owner[${ownerAddress}] is not ADMIN`).to.be.true;
+            expect(await digitoken.connect(owner).hasRole(WHITE_LISTED, owner), `owner[${ownerAddress}] is not WHITELISTED`).to.be.true;
+            expect(await digitoken.connect(owner).hasRole(WHITE_LISTED, user2), `user2[${user2Address}] is WHITELISTED`).to.be.false;
+
+            await digitoken.connect(owner).grantRole(WHITE_LISTED, user2Address);
+            expect(await digitoken.connect(owner).hasRole(WHITE_LISTED, user2), `user2[${user2Address}] has not WHITELISTED by owner[${ownerAddress}]`).to.be.true;
+        });
+    });
+
+    describe("Mint", () => {
+        it("should not allow minter to mint tokens when not whitelisted yet", async () => {
+            const amount = ethers.parseEther("100");
+
+            expect(await digitoken.connect(owner).hasRole(MINTER, minter), `minter[${minterAddress}] is MINTER`).to.be.false;
+
+            await digitoken.connect(owner).grantRole(MINTER, minterAddress);
+            expect(await digitoken.connect(minter).hasRole(MINTER, minterAddress), `minter[${minterAddress}] has not MINTER by owner[${ownerAddress}]`).to.be.true;
+
+            await expect(digitoken.connect(minter).mint(user1Address, amount)).to.be.revertedWith("Sender has not whitelisted");
+        });
+
+        it("should allow minter to mint tokens", async () => {
+            const amount = ethers.parseEther("100");
+
+            expect(await digitoken.connect(owner).hasRole(WHITE_LISTED, minter), `minter[${minterAddress}] is WHITELISTED`).to.be.false;
+
+            await digitoken.connect(owner).grantRole(WHITE_LISTED, minterAddress);
+
+            expect(await digitoken.connect(minter).hasRole(MINTER, minterAddress), `minter[${minterAddress}] has not MINTER by owner[${ownerAddress}]`).to.be.true;
+            expect(await digitoken.connect(minter).hasRole(WHITE_LISTED, minterAddress), `minter[${minterAddress}] has not WHITELISTED by owner[${ownerAddress}]`).to.be.true;
+
+            await digitoken.connect(minter).mint(user1Address, amount);
+
+            expect(await digitoken.balanceOf(user1Address)).to.equal(amount);
+        });
+    });
+
+    describe("Transfer", () => {
+        before(async () => {
+            await digitoken.connect(owner).revokeRole(WHITE_LISTED, user1Address);
+            await digitoken.connect(owner).revokeRole(WHITE_LISTED, user2Address);
+
+            await digitoken.connect(owner).revokeRole(TRANSFER_AGENT, user1Address);
+            await digitoken.connect(owner).revokeRole(TRANSFER_AGENT, user2Address);
+        });
+
+        it("should not allow transfer when Recipient not whitelisted yet", async () => {
+            const amount = ethers.parseEther("100");
+            await expect(digitoken.connect(user1).transfer(user2Address, amount)).to.be.revertedWith("Recipient has not whitelisted");
+        });
+
+        it("should not allow transfer when Sender not whitelisted yet", async () => {
+            const amount = ethers.parseEther("100");
+            await digitoken.connect(owner).grantRole(TRANSFER_AGENT, user2Address);
+            await digitoken.connect(owner).grantRole(WHITE_LISTED, user2Address);
+
+            await expect(digitoken.connect(user1).transfer(user2Address, amount)).to.be.revertedWith("Sender has not whitelisted");
+        });
+
+        it("should transfer when Sender & Reciver are whitelisted", async () => {
+            const amount = ethers.parseEther("100");
+            await digitoken.connect(owner).grantRole(TRANSFER_AGENT, user1Address);
+            await digitoken.connect(owner).grantRole(WHITE_LISTED, user1Address);
+
+            await digitoken.connect(user1).transfer(user2Address, amount);
+            expect(await digitoken.balanceOf(user1Address)).to.equal(ethers.parseEther("0"));
+            expect(await digitoken.balanceOf(user2Address)).to.equal(amount);
+        });
+    });
+});
